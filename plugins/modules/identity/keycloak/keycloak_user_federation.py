1afcab1910104c0553020bbe834a45c7413628e5
Martyn Klassen <lmklassen@gmail.com>
2022-01-13 16:03:31 -0500

    KEYCLOAK: Add custom keycloak user provider support

--- plugins/modules/identity/keycloak/keycloak_user_federation.py
+++ plugins/modules/identity/keycloak/keycloak_user_federation.py
@@ -766,6 +766,10 @@ def main():
         uuidLDAPAttribute=dict(type='str'),
         validatePasswordPolicy=dict(type='bool', default=False),
         vendor=dict(type='str'),
+        updateBaseDn=dict(type='str'),
+        updateUrl=dict(type='str'),
+        updatePrincipal=dict(type='str'),
+        updateCredential=dict(type='str', no_log=True),
     )
 
     mapper_spec = dict(
@@ -783,7 +787,7 @@ def main():
         realm=dict(type='str', default='master'),
         id=dict(type='str'),
         name=dict(type='str'),
-        provider_id=dict(type='str', aliases=['providerId'], choices=['ldap', 'kerberos', 'sssd']),
+        provider_id=dict(type='str', aliases=['providerId'], choices=['ldap', 'kerberos', 'sssd', 'cfmm-password-provider']),
         provider_type=dict(type='str', aliases=['providerType'], default='org.keycloak.storage.UserStorageProvider'),
         parent_id=dict(type='str', aliases=['parentId']),
         mappers=dict(type='list', elements='dict', options=mapper_spec),
